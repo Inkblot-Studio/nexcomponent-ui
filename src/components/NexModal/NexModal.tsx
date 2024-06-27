9c0dd85f5fd1780d58c115d1d123b1279cc8b007
Georgi Drianovski <63720229+GHDryanovski19@users.noreply.github.com>
2024-06-28 00:47:35 +0300

    Major: * Update NexNav to have swiping animation on mobile, rework whole layout * Rework components using react-transition-group to use framer motion for better looks, improved speed, maintenance and overall code complexity

--- src/components/NexModal/NexModal.tsx
+++ src/components/NexModal/NexModal.tsx
@@ -1,50 +1,40 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { motion } from "framer-motion";
 import "./NexModal.scss"
 import { NexModalProps } from "./NexModal.types";
-import { CSSTransition } from "react-transition-group";
 import NexButton from "../NexButton";
 
 const NexModal: React.FC<NexModalProps> = ({ setOpenModal }) => {
-  const [showModal, setShowModal] = useState(false);
-
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setShowModal(true);
-    }, 10);
-
-    return () => clearTimeout(timeoutId);
-  }, []);
-
   const handleContinue = () => {
     setOpenModal(false);
   };
 
   return (
     <div className="nex-modal-wrapper">
-      <CSSTransition
-        in={showModal}
-        classNames="fade"
-        timeout={300}
-        mountOnEnter
-        unmountOnExit
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.3 }}
+        className="nex-modal-inner-wrapper"
       >
-        <div className="nex-modal-inner-wrapper">
-          <div className="nex-modal-body">
-            <p>The next page looks amazing. Hope you want to go there!</p>
-          </div>
-          <div className="nex-modal-footer">
-            <NexButton
-              onClick={() => {
-                setOpenModal(false);
-              }}
-              text="Cancel"
-              type="danger"
-            >
-            </NexButton>
-            <NexButton onClick={handleContinue} text="Continue" type="primary" ></NexButton>
-          </div>
+        <div className="nex-modal-body">
+          <p>The next page looks amazing. Hope you want to go there!</p>
         </div>
-      </CSSTransition>
+        <div className="nex-modal-footer">
+          <NexButton
+            onClick={() => {
+              setOpenModal(false);
+            }}
+            text="Cancel"
+            type="danger"
+          />
+          <NexButton
+            onClick={handleContinue}
+            text="Continue"
+            type="primary"
+          />
+        </div>
+      </motion.div>
     </div>
   );
 }
--- src/components/NexNav/NexNav.tsx
+++ src/components/NexNav/NexNav.tsx
@@ -1,14 +1,33 @@
 import './NexNav.scss';
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { NexNavProps } from './NexNav.types';
 import NexButton from '../NexButton';
-import { CSSTransition } from "react-transition-group";
+import { motion, AnimatePresence } from "framer-motion";
+import { useClickAway } from 'react-use';
 
 const NexNav: React.FC<NexNavProps> = ({ logoSrc, altText, identity, navItems, identityProps }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const ref = useRef(null);
+  
+  useClickAway(ref, (event) => {
+    const target = event.target as HTMLElement;
+
+    if (target.closest('.burger')) {
+      return;
+    }
+
+    setIsMenuOpen(false);
+  });
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
+  };
+
+  const navSwipeAnimation = {
+    initial: { x: '100%', opacity: 0 },
+    animate: { x: 0, opacity: 1 },
+    exit: { x: '100%', opacity: 0, transition: { delay: 0.2 } },
+    transition: { duration: 0.3 },
   };
 
   return (
@@ -35,7 +54,7 @@ const NexNav: React.FC<NexNavProps> = ({ logoSrc, altText, identity, navItems, i
               {identity && (
                 <div className='identity'>
                   <NexButton className='identity-item' text='Login' onClick={identityProps?.onLoginClick} inverted/>
-                  <NexButton className='identity-item' text='Sign Up' onClick={identityProps?.onSignUpClick} inverted/>
+                  <NexButton className='identity-item' text='Sign Up' type='primary' onClick={identityProps?.onSignUpClick} inverted/>
                 </div>
               )}
             </ul>
@@ -49,29 +68,32 @@ const NexNav: React.FC<NexNavProps> = ({ logoSrc, altText, identity, navItems, i
         </div>
       </div>
 
-      <CSSTransition
-        in={isMenuOpen}
-        classNames="fade"
-        timeout={200}
-        mountOnEnter
-        unmountOnExit
-      > 
-        <div className="nex-nav-mobile">
-          <div className="nex-nav-list">
-            {navItems.length && navItems.map((item, index) => (
-                <li key={index} className='nex-nav-item' onClick={item.onClick}> 
+      <AnimatePresence>
+        {isMenuOpen && (
+          <>
+            <motion.div
+              {...navSwipeAnimation}
+              className="nex-nav-mobile"
+              ref={ref}
+              aria-label="Sidebar"
+            >
+            <div className='nex-nav-list'>
+              {navItems.length && navItems.map((item, index) => (
+                <li key={index} className='nex-nav-item' onClick={toggleMenu}>
                   <a className='nex-nav-link'>{item.label}</a>
                 </li>
-            ))}
-          </div>
-          {identity && (
-            <div className='identity'>
-              <NexButton className='identity-item' text='Login' onClick={identityProps?.onLoginClick} inverted/>
-              <NexButton className='identity-item' text='Sign Up' type='primary' onClick={identityProps?.onSignUpClick} inverted/>
+              ))}
+              {identity && (
+                <div className='identity'>
+                  <NexButton className='identity-item' text='Login' onClick={identityProps?.onLoginClick} inverted/>
+                  <NexButton className='identity-item' text='Sign Up' type='primary' onClick={identityProps?.onSignUpClick} inverted/>
+                </div>
+              )}
             </div>
-          )}
-        </div>
-      </CSSTransition>
+          </motion.div>
+          </>
+        )}
+      </AnimatePresence>
     </>
   );
 };
