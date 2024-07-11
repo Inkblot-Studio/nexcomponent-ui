c2dfd94055e41352147ec97c812a6a76463c9e41
Georgi Drianovski <63720229+GHDryanovski19@users.noreply.github.com>
2024-07-11 13:25:45 +0300

    fix: Fix NexLoader props

--- src/components/NexLoader/NexLoader.tsx
+++ src/components/NexLoader/NexLoader.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { NexLoaderProps } from "./NexLoader.types";
 import "./NexLoader.scss"
 
-const NexLoader = () => {
+const NexLoader: React.FC<NexLoaderProps> = ({ size, color }) => {
   return (
     <svg className="nex-loader" viewBox="0 0 50 50">
         <circle className="path" cx="25" cy="25" r="20" />
